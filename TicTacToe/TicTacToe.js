1070ef0e2b4d530b352976ee920cb0f7dc09a636
firstomars <78331647+firstomars@users.noreply.github.com>
2021-05-18 04:13:21 +0000

    Deploying to gh-pages from  @ a6cd0fe21d3572bde6e0c6c2cd82d878aedee621 🚀

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -34,8 +34,8 @@ var Module = typeof Module !== 'undefined' ? Module : {};
       } else {
         throw 'using preloaded data can only be done on a web page or in a web worker';
       }
-      var PACKAGE_NAME = '../../../bin/Emscripten/HelloRaylib/HelloRaylib.data';
-      var REMOTE_PACKAGE_BASE = 'HelloRaylib.data';
+      var PACKAGE_NAME = '../../../bin/Emscripten/TicTacToe/TicTacToe.data';
+      var REMOTE_PACKAGE_BASE = 'TicTacToe.data';
       if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
         Module['locateFile'] = Module['locateFilePackage'];
         err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
@@ -163,10 +163,10 @@ var Module = typeof Module !== 'undefined' ? Module : {};
             for (var i = 0; i < files.length; ++i) {
               DataRequest.prototype.requests[files[i].filename].onload();
             }
-                Module['removeRunDependency']('datafile_../../../bin/Emscripten/HelloRaylib/HelloRaylib.data');
+                Module['removeRunDependency']('datafile_../../../bin/Emscripten/TicTacToe/TicTacToe.data');
 
       };
-      Module['addRunDependency']('datafile_../../../bin/Emscripten/HelloRaylib/HelloRaylib.data');
+      Module['addRunDependency']('datafile_../../../bin/Emscripten/TicTacToe/TicTacToe.data');
     
       if (!Module.preloadResults) Module.preloadResults = {};
     
@@ -187,7 +187,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/assets/.gitkeep", "start": 0, "end": 0, "audio": 0}], "remote_package_size": 0, "package_uuid": "220f76c5-ccfd-4fa9-81d0-be8245b27245"});
+   loadPackage({"files": [{"filename": "/assets/.gitkeep", "start": 0, "end": 0, "audio": 0}], "remote_package_size": 0, "package_uuid": "fa63c5e9-61df-4d41-8ca3-adcd32e8e434"});
   
   })();
   
@@ -1698,7 +1698,7 @@ function createExportWrapper(name, fixedasm) {
   };
 }
 
-var wasmBinaryFile = 'HelloRaylib.wasm';
+var wasmBinaryFile = 'TicTacToe.wasm';
 if (!isDataURI(wasmBinaryFile)) {
   wasmBinaryFile = locateFile(wasmBinaryFile);
 }
